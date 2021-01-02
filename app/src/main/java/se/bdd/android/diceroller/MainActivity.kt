225a85d3064db2edb1162d1f37ab1c96014ae1d2
Joran Stark <joran.stark@gmail.com>
2021-01-02 13:15:56 +0100

    Replace dice with a numerical output

--- app/src/main/java/se/bdd/android/diceroller/MainActivity.kt
+++ app/src/main/java/se/bdd/android/diceroller/MainActivity.kt
@@ -2,19 +2,21 @@ package se.bdd.android.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewAnimationUtils
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     val dice1 = Dice()
-    lateinit var dice: ImageView
+    lateinit var dice: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        dice = findViewById(R.id.imageView)
+        dice = findViewById(R.id.dice)
         val btn1: Button = findViewById(R.id.button1)
         btn1.setOnClickListener{
             rollDice()
@@ -22,15 +24,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        when(dice1.roll()){
-            1 -> dice.setImageResource(R.drawable.dice_1)
-            2 -> dice.setImageResource(R.drawable.dice_2)
-            3 -> dice.setImageResource(R.drawable.dice_3)
-            4 -> dice.setImageResource(R.drawable.dice_4)
-            5 -> dice.setImageResource(R.drawable.dice_5)
-            6 -> dice.setImageResource(R.drawable.dice_6)
-        }
+        // get the center for the clipping circle
+        val cx = dice.width / 2
+        val cy = dice.height / 2
 
+        // get the final radius for the clipping circle
+        val finalRadius = Math.hypot(cx.toDouble(), cy.toDouble()).toFloat()
+
+        // create the animator for this view (the start radius is zero)
+        val anim = ViewAnimationUtils.createCircularReveal(dice, cx, cy, 0f, finalRadius)
+        // make the view visible and start the animation
+        dice.visibility = View.VISIBLE
+        dice.text = dice1.roll().toString()
+        anim.setDuration(200)
+            .start()
     }
 
 
